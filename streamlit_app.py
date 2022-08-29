2a6210090c97902312b1dabda443f93785109993
Juuso Heinonen <user@example.com>
2022-08-29 19:41:54 +0300

    show Fruit Advice as table

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,5 +17,9 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
 
 streamlit.header("Fruityvice Fruit Advice!")
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response.json())
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "watermelon")
+
+# Normalize the json as Pandas dataframe.
+fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
+# Display the Pandas dataframe.
+streamlit.dataframe(fruityvice_normalized)

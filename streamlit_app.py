584f70fb7e03896d6adb0616b9c958e8add4f2f3
Juuso Heinonen <user@example.com>
2022-08-30 11:47:12 +0300

    add prompt

--- streamlit_app.py
+++ streamlit_app.py
@@ -22,7 +22,7 @@ streamlit.header("Fruityvice Fruit Advice!")
 try:
     fruit_choice = streamlit.text_input('What fruit would you like information about?')
     if not fruit_choice:
-        streamlit.error()
+        streamlit.error('Please select fruit to get information')
     else:
         fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
         # Normalize the json as Pandas dataframe.
